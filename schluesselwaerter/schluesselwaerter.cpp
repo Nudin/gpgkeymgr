b54c40f99333b8c4f2b23ee9fda502ad5d3f4002
Michael F. Schönitzer <michael@schoenitzer.de>
2011-07-25 23:28:02 +0200

    +improved backupfunction:  +now tests if given path exists – if not creates directory  +tests if given path is directory

--- schluesselwaerter/schluesselwaerter.cpp
+++ schluesselwaerter/schluesselwaerter.cpp
@@ -46,7 +46,7 @@ string shortenuid(string longuid);
 int searchvector(vector<string> str, string key);
 int readvector(string file, vector<string>& vector);
 string replace_string(string input, const string &search, const string &replace);
-int mycopy(string dir, string filename, string destination);
+int copyfile(string dir, string filename, string destination);
 int backup();
 int remove_key(gpgme_ctx_t ctx, gpgme_key_t key);
 void print_key(gpgme_key_t key);
@@ -380,36 +380,64 @@ Will copy a <home>/dir/filename to destination/filename
 equivalent to `cp ~/$dir/$filename $destination/filename` on unix
 destination must already exist
 */
-int mycopy(string dir, string filename, string destination)
+int copyfile(string dir, string filename, string destination)
 {
    // Get home-Directory
    struct passwd *pw = getpwuid(getuid());
    const string homedir = pw->pw_dir;
    // Put filenames together
    string full_filename = homedir + dir + filename;
-   if ( destination.substr(destination.length(),0) != "/" )
-	destination = destination + "/";
    destination = replace_string(destination,"~",homedir);
-   string full_destination = destination + filename;
+   string full_destination = destination;
+   if ( full_destination.substr(full_destination.length(),0) != "/" )
+	full_destination += "/";
+   full_destination += filename;
 
-   struct stat stFileInfo;
+   struct stat inFileInfo,outFileInfo,pathFileInfo;
    int instat, outstat;
-   instat = stat(full_filename.c_str(),&stFileInfo); // Test if source-file exists
-   outstat = stat(full_destination.c_str(),&stFileInfo); // Test if output-file already exists
+   instat = stat(full_filename.c_str(),&inFileInfo); // Test if source-file exists
+   if(instat != 0) {
+      cerr << gettext("failed to open file: ") << full_filename << endl;
+      return 1;
+   }
+
+   // Now, test path, to which should be written
+   int pathstat = stat(destination.c_str(), &pathFileInfo);
+   if ( pathstat != 0 ) {	// path Does not exist.
+      int success;
+      if ( !yes )
+         if ( !ask_user(gettext("Directory does not exist. Create?")) )
+            return 1;
+      #ifdef __MSDOS__
+         success = mkdir(destination.c_str());
+      #else  /* Unix */
+         success = mkdir(destination.c_str(), 0777);
+      #endif
+
+      if ( success ) {
+         cerr << gettext("Can't create directory.\n");
+         return 1;
+      }
+   }
+   else {
+      int filetyp = pathFileInfo.st_mode & S_IFMT;
+      if ( filetyp != S_IFDIR ) {	// path isn't a directory
+         cerr << gettext("no directory") << endl;
+         return 1;
+      }
+   }
+
+   // Test if file already exists
+   outstat = stat(full_destination.c_str(),&outFileInfo);
    if ( outstat == 0 )
       if ( !yes )
          if ( !ask_user(gettext("File ") + full_destination + gettext(" already exists, overwrite?")) )
             return 1;
-   if(instat == 0) {
-      ifstream ifs(full_filename.c_str(), ios::binary);
-      ofstream ofs(full_destination.c_str(), ios::binary);
-      ofs << ifs.rdbuf();
-      return 0;
-   }
-   else {
-    cerr << gettext("failed to open file: ") << full_filename << endl;
-    return 1;
-    }
+
+   // Copy file in binary mode
+   ifstream ifs(full_filename.c_str(), ios::binary);
+   ofstream ofs(full_destination.c_str(), ios::binary);
+   ofs << ifs.rdbuf();
 }
 
 /*
@@ -422,9 +450,9 @@ int backup()
    cin >> destination;
    if ( destination == "" )
       destination="/backup/";
-   if ( mycopy("/.gnupg/", "pubring.gpg", destination) )
+   if ( copyfile("/.gnupg/", "pubring.gpg", destination) )
       return 1;
-   if ( mycopy("/.gnupg/", "pubring.kbx", destination) )
+   if ( copyfile("/.gnupg/", "pubring.kbx", destination) )
       return 1;
    cout << gettext("Successfully backuped pubring.gpg and pubring.kbx") << endl;
    return 0;
